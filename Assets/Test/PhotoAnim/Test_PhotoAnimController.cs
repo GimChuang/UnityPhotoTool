47c4bf5c0edd6b8149155dc5cac7f4e97c022fe8
gim_razer <rgm79ms@gmail.com>
2019-03-20 17:20:50 +0800

    Modified scripts in submodule gm_PhotoTool and finished Test scenes

---
+++ Assets/Test/PhotoAnim/Test_PhotoAnimController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_PhotoAnimController : MonoBehaviour
+{
+
+    public PhotoAnimController photoAnimController;
+
+    public KeyCode key_playCountDownAnim = KeyCode.A;
+
+    void Start()
+    {
+
+        photoAnimController = FindObjectOfType<PhotoAnimController>();
+        photoAnimController.Init();
+
+    }
+
+    void OnEnable()
+    {
+        PhotoAnimController.OnCountDownFinish += HandleCountDownFinish;
+        PhotoAnimController.OnShotFinish += HandleShotFinish;
+    }
+
+    void OnDisable()
+    {
+        PhotoAnimController.OnCountDownFinish -= HandleCountDownFinish;
+        PhotoAnimController.OnShotFinish -= HandleShotFinish;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(key_playCountDownAnim))
+        {
+            photoAnimController.PlayCountDownAnim();
+        }
+    }
+
+    void HandleCountDownFinish()
+    {
+        Debug.Log("Count Down Finish!");
+        photoAnimController.PlayShotAnim();
+    }
+
+    void HandleShotFinish()
+    {
+        Debug.Log("Shot Finish!");
+    }
+}
---
+++ Assets/Test/PhotoTool/Test_PhotoTool.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test_PhotoTool : MonoBehaviour {
+
+    public int startX = 0;
+    public int startY = 0;
+    public int width = 1080;
+    public int height = 1080;
+
+    public PhotoTool photoTool;
+
+    public RawImage rImg_photo;
+
+    public KeyCode key_takePhoto = KeyCode.B;
+
+    void OnEnable()
+    {
+        PhotoTool.OnPhotoTaken += HandlePhotoTaken;
+    }
+
+    void OnDisable()
+    {
+        PhotoTool.OnPhotoTaken -= HandlePhotoTaken;
+    }
+
+    void Start () {
+
+        photoTool = FindObjectOfType<PhotoTool>();
+        photoTool.Init(startX, startY, width, height);
+
+    }
+
+    void Update () {
+
+        if (Input.GetKeyDown(key_takePhoto))
+        {
+            photoTool.TakePhoto();
+        }
+	}
+
+    void HandlePhotoTaken(Texture2D _takenPhoto)
+    {
+        rImg_photo.texture = _takenPhoto;
+    }
+}
